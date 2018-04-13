4c7bfa8665ed66e34000199b2ef1a99f440931d0
FlorianVaissiere <florian.vaissiere@orange.fr>
2018-04-13 10:19:42 +0200

    test

--- Led/formsLed.py
+++ Led/formsLed.py
@@ -30,72 +30,81 @@ NUMS = [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1,  # 0
 
 REGEXP = r"(^[a-zA-Z]*)"
 
-def show_digit(val, xd, yd):
-	"""show_digit
+class Show(object):
 
-		Calculate position of leds on and off
+	def __init__(self):
 
-		Attributes:
-			val : number display
-			xd  : width display
-			yd  : height display
-	"""
 
-	offset = val * 15
-	for p in range(offset, offset + 15):
-		xt = p % 3
-		yt = (p-offset) // 3
-		ledmatrix.set_pixel( xt+xd, 6-yt-yd, r*NUMS[p], g*NUMS[p], b*NUMS[p])
-	ledmatrix.show()
+	def show_digit(val, xd, yd):
+		"""show_digit
 
-def show_letters_digits(val):
-	"""show_letters_digits
+			Calculate position of leds on and off
 
-		Calculate number of 
+			Attributes:
+				val : number display
+				xd  : width display
+				yd  : height display
+		"""
 
-		Attributes:
-			val : number or letter
-	"""
+		offset = val * 15
+		for p in range(offset, offset + 15):
+			xt = p % 3
+			yt = (p-offset) // 3
+			ledmatrix.set_pixel( xt+xd, 6-yt-yd, r*NUMS[p], g*NUMS[p], b*NUMS[p])
+		ledmatrix.show()
 
-	if re.match(REGEXP, val) is not None:
-		switch = Switcher
-		switch.call_letters(val)
-		
-	else :
-		value 	= int(val)
-		abs_val = abs(value)
-		tens 	= abs_val // 10
-		units 	= abs_val % 10
-		if (abs_val > 9):
-			show_digit(tens, OFFSET_LEFT, OFFSET_TOP)
-		show_digit(units, OFFSET_LEFT+4, OFFSET_TOP)
+	def show_letters_digits(val):
+		"""show_letters_digits
+
+			Calculate number of 
+
+			Attributes:
+				val : number or letter
+		"""
+
+		if re.match(REGEXP, val) is not None:
+			switch = Switcher
+			switch.call_letters(val)
+			
+		else :
+			value 	= int(val)
+			abs_val = abs(value)
+			tens 	= abs_val // 10
+			units 	= abs_val % 10
+			if (abs_val > 9):
+				show_digit(tens, OFFSET_LEFT, OFFSET_TOP)
+			show_digit(units, OFFSET_LEFT+4, OFFSET_TOP)
 
 class Switcher(object):
-    def call_letters(argument):
+
+	def __init__(self):
+
+    def call_letters(self, argument):
         """call_letters
 
         	Call the  function corresponding to the letter
 
         	Attributes:
-        		argument : Letter
+        		argument 	: 
 
         """
+
         method_name = 'show_' + str(argument)
         # Get the method from 'self'. Default to a lambda.
-        method = getattr(method_name, lambda: "Invalid letter")
+        method = getattr(self, method_name, lambda: "Invalid letter")
         # Call the method as we return it
         return method()
  
-    def show_A():
+    def show_A(self):
         return "January"
  
-    def show_B():
+    def show_B(self):
         return "February"
  
-    def show_C():
+    def show_C(self):
         return "March"
 
-    def show_L():
+    def show_L(self):
     	#Vertical
         ledmatrix.set_pixel( 1 , 1 , r*NUMS[1] , g*NUMS[1] , b*NUMS[1])
         ledmatrix.set_pixel( 1 , 2 , r*NUMS[1] , g*NUMS[1] , b*NUMS[1])
