299ac533ff2341df3a4ff62312be3931d9c9679e
FlorianVaissiere <florian.vaissiere@orange.fr>
2018-04-16 15:55:16 +0200

    Add new style of code

--- Led/formsLed.py
+++ Led/formsLed.py
@@ -5,15 +5,48 @@
 #
 # Based on the script to display digits on pi-topPULSE
 
+"""
+MIT License
+Copyright (c) 2018 Florian Vaissiere [https://github.com/FlorianVaissiere]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+###############################################################################
+# Import                                                                      #
+###############################################################################
 import ptpulse
 from ptpulse import ledmatrix
 
 import random
+from random import randrange
 
 import re
+from re import search
 
 import time
+from time import sleep
 
+
+###############################################################################
+# Constant                                                                    #
+###############################################################################
 OFFSET_LEFT = 0
 OFFSET_TOP = 0
 
@@ -28,52 +61,81 @@ NUMS = [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1,  # 0
         1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1,  # 8
         1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1]  # 9
 
+
+###############################################################################
+# Show Class                                                                  #
+###############################################################################
 class Show(object):
 
     def __init__(self):
-        self.r      = random.randrange(255)
-        self.g      = random.randrange(255)
-        self.b      = random.randrange(255)
+        self.r = random.randrange(255)
+        self.g = random.randrange(255)
+        self.b = random.randrange(255)
 
-    def show_digit(self, val, xd, yd):
+    def show_digit(self, val: int, xd: int, yd: int) -> None:
         """show_digit
 
             Calculate position of leds on and off
 
             Attributes:
-                val : number display
-                xd  : width display
-                yd  : height display
+               : int <val> : number display
+               : int <xd>  : width display
+               : int <yd>  : height display
         """
 
         offset = val * 15
         for p in range(offset, offset + 15):
             xt = p % 3
             yt = (p-offset) // 3
-            ledmatrix.set_pixel( xt+xd, 6-yt-yd, self.r*NUMS[p], self.g*NUMS[p], self.b*NUMS[p])
+            ledmatrix.set_pixel( 
+                xt+xd, 
+                6-yt-yd, 
+                self.r*NUMS[p], 
+                self.g*NUMS[p], 
+                self.b*NUMS[p]
+            )
         ledmatrix.show()
 
-    def show_letters_digits(self, val):
+    def show_letters_digits(self, val) -> None:
         """show_letters_digits
 
-            Calculate number of 
+            Make the diffrence between int and str and call different function
 
             Attributes:
-                val : number or letter
+               : str or int <val> : number or letter
         """
 
         valStr = str(val)
+
         if re.search(valStr, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
-            
+            switch = Switcher()
+            switch.call_letters(val)
         else :
             value   = int(val)
             abs_val = abs(value)
             tens    = abs_val // 10
             units   = abs_val % 10
             if (abs_val > 9):
-                self.show_digit(tens, OFFSET_LEFT, OFFSET_TOP)
-            self.show_digit(units, OFFSET_LEFT+4, OFFSET_TOP)
-    
+                self.show_digit(
+                    tens, 
+                    OFFSET_LEFT, 
+                    OFFSET_TOP
+                )
+
+            self.show_digit(
+                units, 
+                OFFSET_LEFT+4, 
+                OFFSET_TOP
+            )
+
+    def clean_display(self) -> None:
+        """clean_display
+
+            Function of ledmatrix to remove the old display
+
+        """
+        ledmatrix.clear()
+        ledmatrix.show()
 
 
 ###############################################################################
@@ -93,39 +155,29 @@ if __name__ == '__main__':
             val = val + 1
 
         if val > 99 and val < 104:
-            ledmatrix.clear()
-            ledmatrix.show()
+            led.clean_display()
             if val == 100:
                 led.show_letters_digits('A')
                 time.sleep(1)
-                ledmatrix.clear()
-                ledmatrix.show()
 
             if val == 101:
                 led.show_letters_digits('B')
                 time.sleep(1)
-                ledmatrix.clear()
-                ledmatrix.show()
 
             if val == 102:
                 led.show_letters_digits('C')
                 time.sleep(1)
-                ledmatrix.clear()
-                ledmatrix.show()
 
             if val == 103:
                 led.show_letters_digits('L')
                 time.sleep(1)
-                ledmatrix.clear()
-                ledmatrix.show()
+
             val = val + 1       
         if val >= 104:
-            ledmatrix.clear()
-            ledmatrix.show()
+            led.clean_display()
             val = 0 
             led.show_letters_digits(val)
             time.sleep(0.1)
             val = val + 1
 
-    ledmatrix.clear()
-    ledmatrix.show()
+    led.clean_display()
