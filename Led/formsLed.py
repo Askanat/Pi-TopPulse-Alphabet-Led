b0d83b2cfc6a9223e1ac6f976ac27ce7753dbcc4
FlorianVaissiere <florian.vaissiere@orange.fr>
2018-04-18 13:48:51 +0200

    Fix style

--- Led/formsLed.py
+++ Led/formsLed.py
@@ -1,10 +1,6 @@
 #! /usr/bin/python3.6
 # -*- coding: utf-8 -*-
 
-# Display different forms on the pi-topPULSE led matrix
-#
-# Based on the script to display digits on pi-topPULSE
-
 """
 MIT License
 Copyright (c) 2018 Florian Vaissiere [https://github.com/FlorianVaissiere]
@@ -26,6 +22,12 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+"""
+
+
+"""
+    Display different forms on the pi-topPULSE led matrix
+    Based on the script to display digits on pi-topPULSE
 """
 
 ###############################################################################
@@ -104,10 +106,13 @@ LETTERS = [ 1, 1, 1,   1, 0, 1,   1, 1, 1,   1, 0, 1,   1, 0, 1, # A
 """
     Dictionary allowing the choice of the line to read
 """
-ALPHABET = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 
-            'J':9, 'K':10, 'L':11, 'M':12, 'N':13, 'O':14, 'P':15, 'Q':16, 
-            'R':17, 'S':18, 'T':19, 'U':20, 'V':21, 'W':22, 'X':23, 'Y':24, 
-            'Z':25 
+ALPHABET = {
+            letter : nb
+            for letter, nb 
+            in zip(
+                [chr(65+x) for x in range(26)],
+                [x for x in range(26)]
+            )
         }
 
 
