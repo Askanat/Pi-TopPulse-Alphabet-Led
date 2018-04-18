1dcfd7a99a561dd7f8c02ca1fee15edfa49a1de4
FlorianVaissiere <florian.vaissiere@orange.fr>
2018-04-18 12:55:43 +0200

    test to fix an error

--- Led/formsLed.py
+++ Led/formsLed.py
@@ -104,9 +104,11 @@ LETTERS = [ 1, 1, 1,   1, 0, 1,   1, 1, 1,   1, 0, 1,   1, 0, 1, # A
 """
     Dictionary allowing the choice of the line to read
 """
-ALPHABET = {'A':0, 'B':1, 'C':2,'D':3,'E':4,'F':5, 'G':6, 'H':7, 'I':8, 'J':9,
-            'K':10, 'L':11, 'M':12, 'N':13, 'O':14, 'P':15, 'Q':16, 'R':17,
-            'S':18, 'T':19, 'U':20, 'V':21, 'W':22, 'X':23, 'Y':24, 'Z':25 }
+ALPHABET = {'A':0, 'B':1, 'C':2,'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 
+            'J':9, 'K':10, 'L':11, 'M':12, 'N':13, 'O':14, 'P':15, 'Q':16, 
+            'R':17, 'S':18, 'T':19, 'U':20, 'V':21, 'W':22, 'X':23, 'Y':24, 
+            'Z':25 
+        }
 
 
 ###############################################################################
@@ -153,7 +155,7 @@ class Show(object):
                : int <xd>  : width display
                : int <yd>  : height display
         """
-
+        print(val)
         offset = val * 15
         for p in range(offset, offset + 15):
             xt = p % 3
